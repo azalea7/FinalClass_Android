435a2cb2928f35d9c9e1c96e339abb91577dc431
Johnny Lao <jlao4394@gmail.com>
2018-11-30 21:51:37 -0500

    login and push notifications base

--- app/src/main/java/edu/cuny/qc/cs/finalclass/Login.java
+++ app/src/main/java/edu/cuny/qc/cs/finalclass/Login.java
@@ -1,42 +1,88 @@
 package edu.cuny.qc.cs.finalclass;
 
-import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+
+import edu.cuny.qc.cs.finalclass.lib.FireBaseUtil;
+
 public class Login extends AppCompatActivity {
-    WebView webView;
+    protected final String TAG = "Login";
+
+    private WebView webView;
+    private final String[] COOKIE_DOMAINS = {"home.cunyfirst.cuny.edu", "hrsa.cunyfirst.cuny.edu", "cunyfirst.cuny.edu", "cuny.edu"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(getApplicationContext());
-        webView.loadUrl("https://cunyfirst.cuny.edu");
+        webView = new WebView(Login.this);
         WebSettings webSettings = webView.getSettings();
         webView.setWebViewClient(new WebViewClient() {
+            private int visitCounter = 0;
+
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                Log.d("Login", request.getUrl().toString());
-                if (request.getUrl().toString().contains("https://home.cunyfirst.cuny.edu")) {
-                    String cookies = CookieManager.getInstance().getCookie(request.getUrl().toString());
-                    Log.d("Login", cookies);
-                    cookies = CookieManager.getInstance().getCookie("https://cuny.edu");
-                    Log.d("Login", cookies);
+                String url = request.getUrl().toString();
+                if (url.equals("https://home.cunyfirst.cuny.edu/psp/cnyepprd/EMPLOYEE/EMPL/h/?tab=DEFAULT")) {
+                    if (visitCounter == 2) {
+                        uploadUserCookies();
+                        finish();
+                        return true;
+                    } else {
+                        visitCounter++;
+                    }
                 }
 
-                view.loadUrl(request.getUrl().toString());
-                return false;
+                view.loadUrl(url);
+                return true;
             }
         });
 
         webSettings.setJavaScriptEnabled(true);
 
         setContentView(webView);
+
+        CookieManager.getInstance().removeAllCookies(new ValueCallback<Boolean>() {
+            @Override
+            public void onReceiveValue(Boolean value) {
+                webView.loadUrl("https://home.cunyfirst.cuny.edu");
+            }
+        });
+    }
+
+    private void uploadUserCookies() {
+        FireBaseUtil.authUser((authUser) -> {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            HashMap<String, String> cookies = extractCookies();
+            HashMap<String, Object> user = new HashMap<>();
+            user.put("cookies", cookies);
+
+            db.collection("users").document(authUser.getUid()).set(user, SetOptions.merge());
+        });
+    }
+
+    private HashMap<String, String> extractCookies() {
+        CookieManager cookieManager = CookieManager.getInstance();
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        for (String domain : COOKIE_DOMAINS) {
+            String cookies = cookieManager.getCookie(domain);
+
+            hashMap.put(domain, cookies);
+        }
+
+        return hashMap;
     }
 }
