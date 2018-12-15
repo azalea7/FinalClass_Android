e2e409a0e2cf56cdc51eae29f705b8a901998d28
Emily <EmilyLi0313@gmail.com>
2018-12-15 14:41:30 -0500

    removed email

--- app/src/main/java/edu/cuny/qc/cs/finalclass/MainActivity.java
+++ app/src/main/java/edu/cuny/qc/cs/finalclass/MainActivity.java
@@ -2,98 +2,146 @@ package edu.cuny.qc.cs.finalclass;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private FirebaseAuth mAuth;
-//    private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "MainUtil";
 
     Spinner schoolSpinner;
     Spinner subjectSpinner;
     Spinner TermSpinner;
     Spinner CareerSpinner;
-    EditText emailText;
     String schoolValue;
     String majorValue;
-    List<String> college;
-    List<String> major;
+    List<String> college = new ArrayList<>();
+    List<String> major = new ArrayList<>();
+    List<String> term = new ArrayList<>();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    DocumentReference selectedSchool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        college = new ArrayList<>();
+        schoolSpinner = (Spinner) findViewById(R.id.school);
         db.collection("colleges").get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
-                        if (task.isSuccessful()){
-                            for (QueryDocumentSnapshot document : task.getResult()){
-                                college.add((String) document.getData().get("name"));
-                            }
-
-                        }else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            college.add(document.getId()+" - "+document.getData().get("name"));
+//                            college.add((String) document.getData().get("name"));
                         }
-                        schoolSpinner = (Spinner) findViewById(R.id.school);
+
+
                         ArrayAdapter<String> schoolAdapter = new ArrayAdapter<String>(MainActivity.this,
                                 android.R.layout.simple_spinner_item, college);
                         schoolAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         schoolSpinner.setAdapter(schoolAdapter);
-                        schoolValue=schoolSpinner.getSelectedItem().toString();
+                        schoolSpinner.setOnItemSelectedListener(
+                                new AdapterView.OnItemSelectedListener() {
+                                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+                                        Object item = parent.getItemAtPosition(pos);
+                                        schoolValue = item.toString();
+
+                                        selectedSchool = db.collection("colleges").document(schoolValue.split("\\s*-\\s*")[0]);
+                                        major.clear();
+                                        selectedSchool.collection("majors").get().
+                                                addOnCompleteListener(task1 -> {
+                                                    if (task1.isSuccessful()) {
+                                                        for (DocumentSnapshot document : task1.getResult()) {
+                                                            major.add((String) document.getData().get(document.getId()));
+                                                        }
+                                                    } else {
+
+                                                    }
+                                                    subjectSpinner = (Spinner) findViewById(R.id.subject);
+                                                    ArrayAdapter<String> subjectAdapter = new ArrayAdapter<String>(MainActivity.this,
+                                                            android.R.layout.simple_spinner_item, major);
+                                                    subjectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                                                    subjectSpinner.setAdapter(subjectAdapter);
+
+                                                    subjectSpinner.setOnItemSelectedListener(
+                                                            new AdapterView.OnItemSelectedListener() {
+                                                                @Override
+                                                                public void onItemSelected(AdapterView<?> parent1, View view1, int position1, long id1) {
+                                                                    Object maj = parent1.getItemAtPosition(position1);
+                                                                    majorValue = maj.toString();
+                                                                    term.clear();
+                                                                    DocumentReference selectedMajor = selectedSchool.collection("majors").document(majorValue.split("\\s*-\\s*")[0]);
+                                                                    selectedMajor.collection("terminfo").get().
+                                                                            addOnCompleteListener(task2 -> {
+                                                                                if (task2.isSuccessful()){
+                                                                                    for (DocumentSnapshot document : task2.getResult()) {
+                                                                                        term.add(document.getId()+" - "+document.getData().get("name"));
+                                                                                    }
+                                                                                }else{}
+
+                                                                                TermSpinner = (Spinner) findViewById(R.id.term);
+
+                                                                                ArrayAdapter<String> termAdapter =new ArrayAdapter<String>(MainActivity.this,
+                                                                                        android.R.layout.simple_spinner_item, term);
+
+                                                                                termAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                                                                                TermSpinner.setAdapter(termAdapter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                            });
+                                                                }
+
+                                                                @Override
+                                                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                                                }
+                                                            }
+                                                    );
+                                                });
+                                    }
+
+                                    public void onNothingSelected(AdapterView<?> parent) {
+                                        System.out.println("NOTHING SELECTED!!!!!!!!!!!!!!!!!!!!");
+                                    }
+                                });
+                    }
+                    else{
+                        //TODO: else part;
                     }
                 });
 
-        if (schoolValue== "Queens College") {
-            DocumentReference selectedSchool = db.collection("colleges").document("QNS01");
 
-            selectedSchool.collection("majors").get().
-                    addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                for (DocumentSnapshot document : task.getResult()) {
-                                    major.add(document.getId());
-                                }
-                            } else {
+          //  System.out.println("0000000000000000000000000000000000000");
 
-                            }
-                            subjectSpinner = (Spinner) findViewById(R.id.subject);
-                            ArrayAdapter<String> subjectAdapter = new ArrayAdapter<String>(MainActivity.this,
-                                    android.R.layout.simple_spinner_item, major);
-                            subjectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                            subjectSpinner.setAdapter(subjectAdapter);
-                            majorValue = subjectSpinner.getSelectedItem().toString();
-                        }
-                    });
-        }
+
         CareerSpinner = (Spinner) findViewById(R.id.career);
 
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
@@ -102,18 +150,6 @@ public class MainActivity extends AppCompatActivity {
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         CareerSpinner.setAdapter(adapter2);
-
-        TermSpinner = (Spinner) findViewById(R.id.term);
-
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.TermInfo, android.R.layout.simple_spinner_item);
-
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        TermSpinner.setAdapter(adapter3);
-
-        emailText = (EditText) findViewById(R.id.emailtext);
-
 
 
 //        DocumentReference doc=db.collection("colleges").document("QNS01");
@@ -140,11 +176,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void addUserInfo(){
         String college = schoolSpinner.getSelectedItem().toString();
-        String email = emailText.getText().toString().trim();
-        if (!TextUtils.isEmpty(email)){
 
-        }else{
-            Toast.makeText(this, "Please enter an Email!", Toast.LENGTH_LONG).show();
-        }
     }
 }
