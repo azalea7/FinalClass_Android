8196801ace1c9958e68c21a29693d3ce8648502d
Zhenhua Cai <steven.cai1990@hotmail.com>
2018-12-17 11:55:35 -0500

    add delete course feature

--- app/src/main/java/edu/cuny/qc/cs/finalclass/UserInfo.java
+++ app/src/main/java/edu/cuny/qc/cs/finalclass/UserInfo.java
@@ -1,16 +1,22 @@
 package edu.cuny.qc.cs.finalclass;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import edu.cuny.qc.cs.finalclass.lib.FirebaseUtil;
@@ -18,13 +24,18 @@ import edu.cuny.qc.cs.finalclass.lib.FirebaseUtil;
 public class UserInfo extends AppCompatActivity {
     protected final String TAG = "Login";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    TextView tv;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-        tv = (TextView) findViewById(R.id.courses);
 //        String row = "";
+        listView=findViewById(R.id.courselist);
+
+        ArrayList<String> courses=new ArrayList<>();
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,courses);
+        listView.setAdapter(arrayAdapter);
 
         FirebaseUtil.authUser((authUser) -> {
             final DocumentReference docRef = db.collection("users").document(authUser.getUid());
@@ -36,7 +47,6 @@ public class UserInfo extends AppCompatActivity {
                 }
 
                 if (snapshot != null && snapshot.exists()) {
-                    tv.setText("");
                     user.forEach((k,v)->{
                         if(k.length()==5 || k.length()==4){
                                 Map<String,Object> section= (Map<String, Object>) v;
@@ -44,8 +54,9 @@ public class UserInfo extends AppCompatActivity {
                                         +"\nSection Number: " + k
                                         +"\nInstructor: "+ section.get("instructor")
                                         +"\nTime: " + section.get("time")
-                                        +"\nStatus: " + section.get("status") + "\n\n";
-                                tv.append(row);
+                                        +"\nStatus: " + section.get("status") + "\n";
+                            courses.add(row);
+                            arrayAdapter.notifyDataSetChanged();
                         }
                     });
                     Log.d(TAG, "Current data: " + snapshot.getData());
@@ -56,6 +67,21 @@ public class UserInfo extends AppCompatActivity {
 
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                AlertDialog.Builder adb=new AlertDialog.Builder(UserInfo.this);
+                adb.setTitle("Delete?");
+                adb.setMessage("Are you sure you want to delete this section?");
+                final int positionToRemove = position;
+                adb.setNegativeButton("Cancel", null);
+                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        courses.remove(positionToRemove);
+                        arrayAdapter.notifyDataSetChanged();
+                    }});
+                adb.show();
+            }
+        });
         Button addCourse =(Button) findViewById(R.id.addBtn);
         addCourse.setOnClickListener(new View.OnClickListener() {
             @Override
