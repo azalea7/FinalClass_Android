e354e97570b677416884abcf06d0a911386b2d58
Emily <EmilyLi0313@gmail.com>
2018-12-17 12:44:46 -0500

    Add code to delete course in priority queue

--- app/src/main/java/edu/cuny/qc/cs/finalclass/UserInfo.java
+++ app/src/main/java/edu/cuny/qc/cs/finalclass/UserInfo.java
@@ -3,6 +3,7 @@ package edu.cuny.qc.cs.finalclass;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,12 +12,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import edu.cuny.qc.cs.finalclass.lib.FirebaseUtil;
@@ -24,21 +30,24 @@ import edu.cuny.qc.cs.finalclass.lib.FirebaseUtil;
 public class UserInfo extends AppCompatActivity {
     protected final String TAG = "Login";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef;
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-//        String row = "";
+        final String[] tokenID = {""};
         listView=findViewById(R.id.courselist);
-
+        ArrayList<String> secNum = new ArrayList<>();
+        ArrayList<String> information=new ArrayList<>();
         ArrayList<String> courses=new ArrayList<>();
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,courses);
         listView.setAdapter(arrayAdapter);
 
         FirebaseUtil.authUser((authUser) -> {
-            final DocumentReference docRef = db.collection("users").document(authUser.getUid());
+            docRef = db.collection("users").document(authUser.getUid());
+            tokenID[0] = authUser.getUid();
             docRef.addSnapshotListener((snapshot, e) -> {
                 Map<String,Object> user = snapshot.getData();
                 if (e != null) {
@@ -55,6 +64,10 @@ public class UserInfo extends AppCompatActivity {
                                         +"\nInstructor: "+ section.get("instructor")
                                         +"\nTime: " + section.get("time")
                                         +"\nStatus: " + section.get("status") + "\n";
+                                String info=section.get("college")+"&"+section.get("term")+"&"+section.get("course career");
+                                System.out.println(info+"*********************************");
+                                information.add(info);
+                                secNum.add(k);
                             courses.add(row);
                             arrayAdapter.notifyDataSetChanged();
                         }
@@ -71,13 +84,41 @@ public class UserInfo extends AppCompatActivity {
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
                 AlertDialog.Builder adb=new AlertDialog.Builder(UserInfo.this);
                 adb.setTitle("Delete?");
-                adb.setMessage("Are you sure you want to delete this section?");
+                adb.setMessage("Are you sure you want to delete this section "+secNum.get(position)+"?");
+
                 final int positionToRemove = position;
                 adb.setNegativeButton("Cancel", null);
                 adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         courses.remove(positionToRemove);
+                        String sectionID = secNum.get(positionToRemove);
+                        secNum.remove(positionToRemove);
                         arrayAdapter.notifyDataSetChanged();
+                        Map<String,Object> updates = new HashMap<>();
+                        String[] info =information.get(positionToRemove).split("&");
+
+                        updates.put(sectionID, FieldValue.delete());
+                        docRef.update(updates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                            }
+                        });
+                        db.collection("priority1").document(info[0].split("\\s*-\\s*")[0])
+                        .collection(info[1].split("\\s*-\\s*")[0]).document(info[2].split("\\s*-\\s*")[0])
+                        .collection(sectionID).document(tokenID[0])
+                                .delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                            }
+                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error deleting document", e);
+                                    }
+                                });
                     }});
                 adb.show();
             }
